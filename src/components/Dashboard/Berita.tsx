83bdc2f3015411d03e89467eddea99d8cf94cc3f
anashrulyusuf <anashrulyusuf@gmail.com>
2021-07-01 15:49:54 +0700

    to fixed saldo

--- src/components/Dashboard/Berita.tsx
+++ src/components/Dashboard/Berita.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, CardBody } from '@windmill/react-ui'
-import Helper from 'lib/helper';
 import { iContent } from 'lib/interface';
 import Link from 'next/link'
 
@@ -24,30 +23,45 @@ const Cards: React.FC<iCards> = ({dataBerita}) => {
                 <div className="overflow-y-auto" style={{maxHeight:'600px'}}>
                 {
                     dataBerita?.length>0?dataBerita.map((item:iContent,i:number)=>{
-                        return(
-                            <div className="flex" key={i}>
-                                <div className="h-full text-left px-4 py-4 bg-gray-800 w-full justify-end border-t-2 border-gray-900">
-                                    <a className="flex items-center flex-wrap">
-                                        <img
-                                            alt="testimonial"
-                                            className="inline-block object-cover object-center w-16 h-16 mb-4 bg-gray-100 rounded"  
-                                            src={item.picture} />
-                                         <span className="flex flex-col flex-grow pl-4">
-                                            <Link href={`/content/berita/${item.id}`}>
-                                                <span className="font-bold text-sm text-gray-700 dark:text-gray-200 -mt-4">
-                                                    <button className="text-blue-700 dark:text-blue-200">
-                                                        {Helper.removeHtml(item.title.length>40?item.title.substr(0,40)+'..':item.title)} 
-                                                    </button>
-                                                    
-                                                </span>
-                                            </Link>
-                                            <span className="text-xs text-gray-300 font-thin">
-                                            {((item.caption.replace(/<[^>]*>?/gm, '')).replace(/&nbsp;/g, '')).substr(0,120)}
-                                            </span>
-                                        </span>
-                                    </a>
-                                </div>
+                        return (
+                            <div key={i} className="bg-gray-800 w-128 rounded shadow-md flex card text-grey-darkest">
+                                <img className="w-12 h-12 mb-4 mr-2 bg-gray-100 rounded" src={item.picture} alt="Room Image"  />
+                                <Link href={`/content/berita/${item.id}`}>
+                                <div className="w-full flex flex-col cursor-pointer">
+                                    <div className="flex-1">
+                                        <span className="text-xs text-gray-200">{item.title}</span>
+                                        <div className="flex items-center">
+                                            <div className="pr-2 text-xs text-gray-300">
+                                            <div dangerouslySetInnerHTML={{__html: item.caption.length>40?item.caption.substr(0,40)+'..':item.caption}}></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </Link>
                             </div>
+                            // <div className="flex" key={i}>
+                            //     <div className="h-full text-left px-4 py-4 bg-gray-800 w-full justify-start border-t-2 border-gray-900">
+                            //         <a className="flex items-center flex-wrap">
+                            //             <img
+                            //                 alt="testimonial"
+                            //                 className="inline-block object-cover object-center w-16 h-16 mb-4 bg-gray-100 rounded"  
+                            //                 src={item.picture} />
+                            //              <span className="flex flex-col pl-4">
+                            //                 <Link href={`/content/berita/${item.id}`}>
+                            //                     <span className="font-bold text-xs text-gray-700 dark:text-gray-200">
+                            //                         <button className="text-blue-700 dark:text-blue-200">
+                            //                             {Helper.removeHtml(item.title.length>40?item.title.substr(0,40)+'..':item.title)} 
+                            //                         </button>
+                                                    
+                            //                     </span>
+                            //                 </Link>
+                            //                 <span className="text-xs text-gray-300 font-thin">
+                            //                 {((item.caption.replace(/<[^>]*>?/gm, '')).replace(/&nbsp;/g, '')).substr(0,30)}
+                            //                 </span>
+                            //             </span>
+                            //         </a>
+                            //     </div>
+                            // </div>
 
                         );
                     }):<div className="mt-3 pb-2 text-gray-700 dark:text-gray-400 text-center">Tidak ada data.</div>
